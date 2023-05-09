30f18b2294631a2fd5df8b1f3659243712178339
Andrew Johnson <63437142+Ajjme@users.noreply.github.com>
2023-05-09 10:42:31 -0700

    geojson reader

---
+++ Creating_new_shape.R
@@ -0,0 +1,26 @@
+library(rgdal)
+library(sp)
+library(sf)
+library(raster)
+###Loading through a geojson-----------------
+# install.packages("geojsonio")
+
+library(geojsonio)
+ca_cities <- geojson_read("./shapes/City_Boundaries.geojson",  what = "sp")
+
+### doesn't work for me?
+my_sf_all_city_CA <- readOGR("shapes/City_Boundaries.shp")
+saveRDS(my_sf, file = "./shapes/my_sf.rds")
+
+### from vingette---------
+
+usa3 <- raster::getData(country = "USA", level = 3)
+ger2 <- raster::getData(country = "Germany", level = 2)
+
+### reading in an RDS someone made--------------
+if (!require("rspatial")) remotes::install_github('rspatial/rspatial')
+library(rspatial)
+
+davis_polygon <- sp_data('city.rds') #just one city (davis)
+class(davis_polygon )
+
